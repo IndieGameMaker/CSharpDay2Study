b7d0462725024cc4b708e7d5bef119129a28f243
Lee JaeHyun <myleje@gmail.com>
2025-04-16 12:26:02 +0900

    Project init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpDay;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var gameLogic = new GameLogic();
+        gameLogic.StartGame();
+    }
+}
+
+class GameLogic
+{
+    public void StartGame()
+    {
+
+    }
+}
+
+class Player
+{
+
+}
